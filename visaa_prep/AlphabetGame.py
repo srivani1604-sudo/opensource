e974b697de50b5102354c9fcae2e4e8a80ba7820
BandiSrivani <srivanibandi12@gmail.com>
2024-11-19 20:31:52 +0530

    AlphabetGame.py

---
+++ visaa_prep/AlphabetGame.py
@@ -0,0 +1,12 @@
+s=input().strip()
+v="aeiouAEIOU"
+c="bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+c1=0
+v1=0
+for i in s:
+    if i in v:
+        v1+=1
+    elif i in c:
+        c1+=1
+print(v1,end=",")
+print(c1)
